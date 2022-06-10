f5b3396933ee1f783a6bfb37c714bb0c5277e3c6
kkranzfelder <kkranzfelder@gmail.com>
2022-06-10 18:16:20 -0400

    updated dependencies and got productId route and query working. Also created tests file for myself to start jest testing.

--- DB/models.js
+++ DB/models.js
@@ -1,9 +1,35 @@
 const { db } = require ('./index.js');
 
-const getProducts = (productId) => {
-  db.query('SELECT * FROM product WHERE id = 150')
+const getProducts = (pageReq, countReq) => {
+  let page = pageReq || 1;
+  let count = countReq || 5;
+  let offSet = (page * count) - count;
+
+  return db.query(`SELECT * FROM product LIMIT $1 OFFSET $2`, [count, offSet]);
 };
 
+const getProductById = (productId) => {
+  return db.query(
+    `SELECT json_build_object(
+    'id', pd.id,
+    'name', pd.name,
+    'slogan', pd.slogan,
+    'description', pd.description,
+    'category', pd.category,
+    'default_price', pd.default_price,
+    'features', (
+        SELECT json_agg(json_build_object(
+            'feature', f.feature,
+            'value', f.value
+            ))
+        FROM features f WHERE f.product_id = pd.id
+    )
+) AS RESULT
+FROM product pd
+WHERE pd.id = $1;`, [productId]);
+}
+
 module.exports = {
-  placeholder
+  getProducts,
+  getProductById
 }
--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,31 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 
 const SPORT = process.env.SPORT;
-const {db} = require('../DB/index.js');
-const { getProducts } = require('../DB/models.js')
+const { db } = require("../DB/index.js");
+const { getProducts, getProductById } = require("../DB/models.js");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/products', (req, res) => {
-  getProducts()
-  .then((results) => res.send(results))
-  .catch(err => console.log(err));
-})
+app.get("/products", (req, res) => {
+  getProducts(req.query.page, req.query.count)
+    .then((results) => res.send(results))
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(502);
+    });
+});
+
+app.get("/products/:productId", (req, res) => {
+  getProductById(req.params.productId)
+    .then((results) => res.send(results))
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(502);
+    });
+});
 
 app.listen(SPORT, () => {
   console.log(`server running at http://localhost:${SPORT}`);
